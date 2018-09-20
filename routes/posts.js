08a9e9f0fcf4a09897963080000cf93c5f8912df
avivlaban <aviv.laban@me.com>
2018-09-20 15:36:32 +0300

    UpVotes first implementation

--- routes/posts.js
+++ routes/posts.js
@@ -48,13 +48,50 @@ router.put('/update/:postId', async (req, res) => {
     console.log("Post id is: ", req.params.postId);
     console.log("New Title is: ", req.body.title);
     console.log("New Post is: ", req.body.content);
-    post = await Post.findByIdAndUpdate(req.params.postId, 
+    post = await Post.findOneAndUpdate(req.params.postId, 
         {
             title: req.body.title,
             content: req.body.content,
             dateUpdated: Date.now()
         });
     res.send("Post was updated");
+});
+
+router.put('/upvote/post/:postId/user/:userId', async (req, res) => {
+    console.log(1);
+    // Make Sure Post Exists
+    let post = await Post.findById(req.params.postId);
+    console.log(2);
+    // If Not - Return 404
+    if(!post) return res.status(404).send('A post with the given id does not exists');
+    console.log(3);
+    // Make Sure User Exists
+    console.log("User id is: ", req.params.userId);
+    console.log(4);
+    const user = await User.findById(req.params.userId);
+    console.log(5);
+    // If Not - Return 404
+    if(!user) return res.status(404).send('User does not exist and therefore can not upvote');
+    console.log(6);
+    //If Yes - Get Upvotes Array
+    let upVotesList = post.upVotes;
+    console.log(7);
+    const indexOfUserInUpVoteList = req.params.userId;
+    console.log(8);
+    
+    if(indexOfUserInUpVoteList > -1){
+        console.log(`User ${user._id} UpVoting Post ${post._id}.`);
+        upVotesList.put(userId);
+        post.upVotes = upVotesList;
+        post.upVotesCount = upVotesList.length;
+        const result = await post.save();
+        console.log("Post UpVoted Updated:", result);
+        res.send(result);
+    }else{
+        //const errorMessage = `User ${user._id} already UpVoted Post ${post._id}. A user can only do it once`;
+        console.log("Bye");
+        res.send("Bye");
+    }
 });
 
 module.exports = router;
